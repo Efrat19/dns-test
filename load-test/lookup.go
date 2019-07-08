2e6c0d9cccec8223b70e47fee887fcd6d904462a
efrat <efrat@Efrats-MacBook-Air.local>
2019-07-09 02:39:29 +0300

    auto go mod

--- load-test/lookup.go
+++ load-test/lookup.go
@@ -36,7 +36,7 @@ func getFlags() (string, time.Duration) {
 
 func initLogstash() *logstash.Logstash {
 	port, convErr := strconv.Atoi(os.Getenv("LOGSTASH_PORT"))
-	log := logstash.New(os.GetEnv("LOGSTASH_HOST"), port, 5)
+	log := logstash.New(os.Getenv("LOGSTASH_HOST"), port, 5)
 	_, err := log.Connect()
 	if err != nil {
 		fmt.Println(err)
